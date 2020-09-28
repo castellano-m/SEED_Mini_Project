66d8dde70b145f6319128f6a3ba73ac7fb1587aa
madisonheeg <70350500+madisonheeg@users.noreply.github.com>
2020-09-28 13:48:28 -0500

    To Do: reset angPos to zero

--- 4.4_ReadEncoderMotor.ino
+++ 4.4_ReadEncoderMotor.ino
@@ -1,9 +1,10 @@
 /* Name: Madison Heeg
  * Date: 25 Sept 2020
  * 
- * Title: Mini Project - Section 4.4,
+ * Title: Mini Project - Section 4.4 Read Motor Encoder 
  * Setup - One Pololu Motor
- *       - Channel A = pin 2 (ISR); Channel B = pin 13
+ *       - Channel A (yellow) = pin 2 (ISR); Channel B (white) = pin 13
+ *       - Vcc (blue) = 5 V on Arduino
  *   
  * Functionality
  *  - Use high perforamnce encoder library to read the encoder
@@ -14,13 +15,17 @@
  *  - Declaration               Encoder myEnc(pinISR, pin2) 
  *  - myEnc.read()              returns accumulated position (signed)     
  *  - myEnc.write(newPosition)  set the accumlated position to a new number
+ *  
+ * TO DO: 
+ *  - add functionality to this code such that angular position can be re-set to zero  
+ *  
  */
 
 #include <Encoder.h>  // Downloaded from https://www.pjrc.com/teensy/td_libs_Encoder.html 
 
 /************************************* VARIABLE DECLARATIONS *************************************/ 
 const int pinA = 2; const int pinB = 13;        // ChannelA = pin2(ISR) & ChannelB = pin13
-// const int N = # of turns per one revolution
+const int N = 3195;                             // # of turns per one revolution, experimentally determined
 
 Encoder encoder(pinA,pinB);                     // initializing the encoder library 
 
@@ -39,7 +44,7 @@ void loop() {
   long countNew = encoder.read(); 
   
   if (countNew != countPrev) {
-    // angularPos = 2*PI*(double)countNew/N
+    angularPos = 2.0*PI*(double)countNew/(double)N;
     Serial.print("Position: "); Serial.print(countNew);
     Serial.print(", Angular Position: "); Serial.println(angularPos);   
     countPrev = countNew; 
